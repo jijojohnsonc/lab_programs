eb3a51bd398f7637ae9b1364b7812eadd317efd3
Jijo Johnson <67041745+jijojohnsonc@users.noreply.github.com>
2021-09-22 14:23:48 +0530

    Add files via upload

--- OS Lab/Paging/paging.c
+++ OS Lab/Paging/paging.c
@@ -3,18 +3,47 @@
 void fcfs(int page[], int n, int frame[], int size)
 {
     int i;
+    int first_in[size];
     for (i = 0; i < size; i++)
     {
         frame[i] = page[i];
+        first_in[i] = frame[i];
+        printf("Miss\n");
         for (x = 0; x <= i; x++)
             printf("%d\t", frame[x]);
-        prinf()
+        printf("\n");
     }
-    i = 0;
-    int j, no_page_faults;
+    int j, no_of_page_faults = i;
     for (j = size; j < n; j++)
     {
-
+    	int flag = 0;
+    	for (int k = 0; k < i; k++)
+    	{
+    		if (page[i] == frame[k])
+    		{
+    			flag = 1;
+    			printf("Hit\n");
+    		}
+    		if (!flag)
+    		{
+    		no_of_page_faults++;
+    			printf("Miss\n");
+    			int l;
+    			for (l = 0; l < k; l++)
+    			{
+    				if (frame[l] == first_in[0])
+    				{
+    					for (m = 1; m < size; m++)
+    						first_in[m - 1] = first_in[m];
+    					frame[l] = page[i];
+    					first_in[size - 1] = page[i];
+    					i = (i + 1) % size;
+    				}	
+    			}
+    		}
+    		for (x = 0; x < size; x++)
+            		printf("%d\t", frame[x]);
+    	}
     }
 }
 
@@ -39,18 +68,18 @@ void main()
 		printf("\n1) FIFO\n");
 		printf("2) LRU\n");
 		printf("3) LFU\n");
-        printf("4) Exit\n");
-        printf("Enter your choice : ");	
+        		printf("4) Exit\n");
+        		printf("Enter your choice : ");	
 		scanf("%d", &opt);
 		if (opt == 1)
-        {
-
-        }
+        		{
+			fcfs(page, n, frame, size);
+        		}
 		else if (opt == 2)
-        {
-
-        }
-        else if (opt == 3)
+        		{
+;
+        		}
+        		else if (opt == 3)
 			break;
 		else
 			printf("Invalid entry");
